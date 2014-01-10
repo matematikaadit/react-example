8ae513ec703fd4659e53b2485a68d39ada9cb1f5
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-01-11 06:15:40 +0700

    tambah bblist generator

---
+++ bblist/src/bblist.js
@@ -0,0 +1,71 @@
+/**
+ * @jsx React.DOM
+ */
+
+var BBList = React.createClass({
+    /* Main Component, our BBList app that will be mounted as the root of
+     * the main application. Host the input and output.
+     */
+
+    render: function() {
+        /* Just render the input and output, also give a handle change in
+         * input and reflect it in the output.
+         */
+        var nodes = [];
+        for (var i = 0; i < 3; i++) {
+            nodes.push(
+                <input type="text"
+                    onChange={this.handleOnChange.bind(this, i)}
+                    key={i}
+                    ref={"input" + i} />
+            );
+            nodes.push(<br />);
+        }
+        return (
+            <div className="bblist">
+                <div className="input">
+                    <h3>Input</h3>
+                    {nodes}
+                </div>
+                <h3>Output</h3>
+                <textarea className="output"
+                    value={this.state.output} />
+            </div>
+        );
+    },
+
+    getInitialState: function() {
+        /* Initial state of the app. Output is empty. list is also empty.
+        */
+        return {output: '', list: []};
+    },
+
+    handleOnChange: function(i) {
+        /* Handle input changing, then update the output state
+        */
+        var value = this.refs["input"+i].getDOMNode().value.trim();
+        var list = this.state.list;
+        list[i] = value;
+        output = this.convertToBBList(list);
+        this.setState({output: output, list: list});
+    },
+
+    convertToBBList: function(list) {
+        /* A Converter from js Array to a BBList code
+         */
+         output = "[LIST]\n"
+         for (var i = 0; i < list.length; i++) {
+             var item = "";
+             if (list[i]) { item = list[i] }
+             output += "[*]" + item + "\n"
+         }
+         output += "[/LIST]\n"
+         return output;
+    },
+
+});
+
+React.renderComponent(
+    <BBList />,
+    document.getElementById('content')
+);
