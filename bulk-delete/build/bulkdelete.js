debcdde14e4848403b94065da86ec92d2f98b6f9
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-01-26 18:36:47 +0700

    BulkDeleteApp init

---
+++ bulk-delete/build/bulkdelete.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+
+var BulkDeleteApp = React.createClass({displayName: 'BulkDeleteApp',
+    render: function() {
+        return (
+            React.DOM.div( {className:"BulkDeleteApp"}
+            )
+        );
+    },
+});
+
